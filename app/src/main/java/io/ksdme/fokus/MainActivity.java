ef0ebab839f42f11abc4d43077e7ede78ffd0ed5
Kilari Teja <kilariteja9@gmail.com>
2017-08-20 17:02:23 +0530

    Initial Design

--- app/src/main/java/io/ksdme/fokus/MainActivity.java
+++ app/src/main/java/io/ksdme/fokus/MainActivity.java
@@ -1,5 +1,6 @@
 package io.ksdme.fokus;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* make up */
+        makeActionBar();
+
+    }
+
+    private void makeActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
     }
 }
